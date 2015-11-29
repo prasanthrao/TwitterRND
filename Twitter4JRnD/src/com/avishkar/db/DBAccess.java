7fb52f559d38ab78e000dafac1812a6e0bf82783
Prasanth Rao <uvce.prasanth@gmail.com>
2015-11-29 11:20:46 +0530

    Refactoring Changes
    Change-Id: If7b8d048e1872e03b869b77479917ad0427fbcac

--- Twitter4JRnD/src/com/avishkar/db/DBAccess.java
+++ Twitter4JRnD/src/com/avishkar/db/DBAccess.java
@@ -13,6 +13,9 @@ import com.mongodb.util.JSON;
 
 public class DBAccess {
 	
+	private static final int DB_PARAM_PORT = 27017;
+	private static final String DB_PARAM_LOCALHOST = "localhost";
+	
 	private static HashMap<String,DBCollection> collection = new HashMap<>();
 	private static String dbName = "twitter";
 	static DB db = null;
@@ -20,7 +23,7 @@ public class DBAccess {
 	protected static DBCollection getDBConnection(String collectionName) throws UnknownHostException{
 		if(!collection.containsKey(dbName+collectionName)){
 			if(db==null){
-				Mongo mongo = new MongoClient("localhost", 27017);
+				Mongo mongo = new MongoClient(DB_PARAM_LOCALHOST, DB_PARAM_PORT);
 				db = mongo.getDB(dbName);
 			}
 			collection.put(dbName+collectionName, db.getCollection(collectionName));
